4f844fe157499e0ba864b7d5a2a40063fb393fe6
Jayasree <jspa9999@gmail.com>
2024-10-14 11:59:05 +0530

    Add files via upload

---
+++ Bahubali.js
@@ -0,0 +1,11 @@
+let movieName = "Bahbali2";
+let movieDirector = "Rajamouli";
+let movieCast =["Prabas","Anushka","Tammana"];
+let movieBudget = 8000000000000;
+
+let aboutBahubali=()=>{
+    console.log("Bahubali2 is one of the biggest cinemas in India ");
+
+}
+
+module.exports = {movieName,movieDirector,movieCast,aboutBahubali};
---
+++ server.js
@@ -0,0 +1,10 @@
+let {movieName,movieDirector,movieCast,aboutBahubali} = require("./Bahubali");
+
+//console.log(BahubaliMovieObj.movieName);
+//console.log(BahubaliMovieObj.movieDirector);
+//console.log(BahubaliMovieObj.movieCast);
+//BahubaliMovieObj.aboutBahubali();
+console.log(movieName);
+aboutBahubali();
+console.log(movieDirector);
+console.log(movieCast);
